ac79ad52da91566d42aaa4e9c27fccf12fae9abc
nkport <ktmtworks@outlook.jp>
2024-01-19 16:31:11 +0900

    「静的なページを表示しよう」課題提出

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+// ルーティングを設定するコントローラの宣言
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index']);
